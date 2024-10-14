5d2633928145649dcb179b93681a3df0f33c2989
Yugal <yugalramesh6832@gmail.com>
2024-10-14 11:48:54 +0530

    Added

--- middleware.js
+++ middleware.js
@@ -26,36 +26,163 @@
 // Application Level Middleware end
 
 
+
+
+
+
+
+
+
+
+
 // Route Level Middleware start
 
-const express = require('express');
-const router = express.Router();
+// const express = require('express');
+// const router = express.Router();
 
-router.use((req, res, next) => {
-    console.log('Router-Level Middleware 1');
-    next();
-});
+// router.use((req, res, next) => {
+//     console.log('Router-Level Middleware 1');
+//     next();
+// });
 
-router.use((req, res, next) => {
-    console.log('Router-Level Middleware 2');
-});
+// router.use((req, res, next) => {
+//     console.log('Router-Level Middleware 2');
+// });
 
-router.get('/users', (req, res) => {
-    res.send('User list');
-    console.log("User List");
-});
+// router.get('/users', (req, res) => {
+//     res.send('User list');
+//     console.log("User List");
+// });
 
-router.get('/abc', (req, res) => {
-    res.send('ABC list');
-    console.log("ABC List");
-});
+// router.get('/abc', (req, res) => {
+//     res.send('ABC list');
+//     console.log("ABC List");
+// });
 
-const app = express();
-app.use('/api', router); 
+// const app = express();
+// app.use('/api', router); 
 
-app.listen(4444, () => {
-    console.log('Server running on port : 4444');
-});
+// app.listen(4444, () => {
+//     console.log('Server running on port : 4444');
+// });
 
 
 // Route Level Middleware end
+
+
+
+
+
+
+
+
+
+
+
+// Error-Handling Middleware start
+
+// const express = require('express');
+// const app = express();
+
+// const port = 4444;
+
+// Application Level Middleware
+// app.use((req,res,next)=>{
+//     console.log('Application Level Middleware !');
+//     next();
+// })
+
+// simple middleware
+// app.get('/',(req,res)=>{
+//     console.log("Home Page !");
+//     res.send("Home Page !")
+// })
+
+// simple middleware
+// app.get('/about',(req,res,next)=>{
+//     const err = new Error("Error While About Page !");
+//     err.status = 400;
+//     next(err);
+// })
+
+// error middleware 1 
+// if first error midd. is not work so go to the second err. midd.
+// app.use((err,req,res,next) =>{
+//     if(err.status === 500){
+//         console.log('Error While About Page !');
+//         res.status(500).send("Internal Server Error !");
+//     }
+//     else{
+//         next(err);
+//     }
+// })
+
+// error middleware 2
+// app.use((err,req,res,next)=>{
+//     console.log('Unknown Errorss  :',err.message);
+//     res.status(500).send("Something Went Wrong !");
+// })
+
+// app.listen(port,()=>{
+//     console.log(`Server Running On Port : ${port}`);
+// })
+
+// Error-Handling Middleware end
+
+
+
+
+
+
+
+
+
+
+// Built-in Middleware start
+
+// with express.json() start
+
+// const express = require('express');
+// const app = express();
+
+// const port = 4444;
+
+// app.use(express.json());
+
+// app.get('/',(req,res)=>{
+//     console.log("Built_in Middleware !");
+//     const {name} = req.body;
+//     res.send(`Your Name is ${name}!`);
+//     console.log(`Your Name is ${name}!`);
+// })
+
+// app.listen(port,()=>{
+//     console.log(`Server Running On Port : ${port}`);
+// })
+
+// with express.json() end
+
+
+// with express.urlencoded() start
+
+// const express = require('express');
+// const app = express();
+
+// const port = 4444;
+
+// app.use(express.urlencoded({extended:true}));
+
+// app.get('/',(req,res)=>{
+//     console.log("Built_in Middleware !");
+//     const {Name,Surname} = req.body;
+//     res.send(`Your Name is ${Name} ${Surname}!`);
+//     console.log(`Your Name is ${Name} ${Surname}!`);
+// })
+
+// app.listen(port,()=>{
+//     console.log(`Server Running On Port : ${port}`);
+// })
+
+// with express.urlencoded() end
+
+// Built-in Middleware end
